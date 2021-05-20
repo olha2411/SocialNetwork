f0f42204d02204f5b8fccbe472697919d3443348
Olha Pavlushchenko <pavluschenko2003@gmail.com>
2021-05-20 13:46:59 +0300

    last

--- SocialNetworkLib/User.cs
+++ SocialNetworkLib/User.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace SocialNetworkLib
 {
     public abstract class User
-    {
-        
+    {        
         public int Id { get; set; }
 
         public string Name { get; set; }
         
+
         public virtual List<LoggedUser> ShowUsers(List<LoggedUser> RegisteredUsers, int LoggedUserId)
         {
             List<LoggedUser> Users = new List<LoggedUser>();
